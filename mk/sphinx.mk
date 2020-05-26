f49519baf9a114457e1e5c30eace927df59f6881
Roie Black <roie.black@gmail.com>
2020-05-26 10:27:39 -0500

    tweaks to pylit code

--- mk/sphinx.mk
+++ mk/sphinx.mk
@@ -7,3 +7,10 @@ docs:	changes	## run Sphinx to build html pages
 .PHONY:	docs-clean
 docs-clean:
 		rm -rf rst/_build/doctrees
+
+.PHONY: latex
+latex:
+	pip install . && \
+	cd rst && \
+	sphinx-build -b latex -d _build/doctrees . _build/latex
+
