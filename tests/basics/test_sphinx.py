8e6d8bdff650e759dc497daa93a1627c39c0982f
Roie Black <roie.black@gmail.com>
2020-06-01 15:12:32 -0500

    organizing tests

--- tests/basics/test_sphinx.py
+++ tests/basics/test_sphinx.py
@@ -15,3 +15,18 @@ def test_sphinx_basic_latex(app, status, warning):
     print("TEXPATH: ", texpath)
     tex = open(texpath).read()
     assert 'sphinxcontrib.pylit' in tex
+
+def test_extension_import():
+    """verify extension can be imported and has a setup function"""
+    try:
+        mod = import_module(PACKAGE)
+    except ImportError:
+        return False
+    setup = getattr(mod, 'setup', None)
+    assert not setup is None
+
+
+@pytest.mark.sphinx('html', testroot=ROOTDIR, freshenv=True)
+def test_sphinx_basic_html(app, status, warning):
+    """verify sphinx runs with new extension"""
+    app.builder.build_all()
