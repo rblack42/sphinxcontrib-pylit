6bf74c3868f7e48cbce77b5b93f54d83d947b20b
Roie Black <rblack@austincc.edu>
2020-06-01 07:23:22 -0500

    working on data store

--- sphinxcontrib/pylit/DataStore.py
+++ sphinxcontrib/pylit/DataStore.py
@@ -13,3 +13,36 @@ class DataStore(object):
 
     def delete(self):
         shutil.rmtree(self.rootdir)
+        
+    def add_code_block(self, blktype, tag, docname, lineno, content):
+        # generate block hash
+        nbytes = len(content)
+        bdata = blktype + str(nbytes) + "\0" + content
+        edata = bdata.encode('utf-8')
+        m = hashlib.sha1()
+        m.update(edata)
+        blkhash = m.hexdigest()
+
+        # write block to repo
+        blkdir = blkhash[:2]
+        os.makedirs(os.path.join(self.rootdir,blkdir))
+        blkname = blkhash[2:]
+        blkpath = os.path.join(self.rootdir, blkdir, blkname)
+        if os.path.isfile(blkpath):
+            print("Block already in store: ", tag)
+            return
+        print("Block stored in ", blkdir, blkname)
+        with open(blkpath,'w') as fout:
+            fout.write(edata)
+
+        # record block tag in catalog
+        treeitem = \
+                'tree' + ":" + \
+                blkhash + ":" + \
+                tag + ":" + \
+                docname + ":" + \
+                str(lineno) + "\n"
+        catalog = os.path.join(self.rootdir, 'catalog')
+        with open(catalog,"a") as c:
+            c.write(treeitem)
+
--- sphinxcontrib/pylit/PylitCodeBlock.py
+++ sphinxcontrib/pylit/PylitCodeBlock.py
@@ -1,11 +1,22 @@
 from sphinx.directives.code import CodeBlock
+from .DataStore import DataStore
 
 
 class PylitCodeBlock(CodeBlock):
 
     def run(self):
         caption = self.options.get('caption')
+        if not caption:
+            caption = ""
         newcaption = '<<' + caption + '>>=='
         self.options['caption'] = newcaption
 
+        # store this block if needed
+        ds = DataStore()
+        docname = self.env.docname
+        lineno = self.lineno
+        content = '\n'.join(self.content)
+        ds.add_code_block('code', caption, docname, lineno, content)
+
+        # format the block and return
         return super(PylitCodeBlock, self).run()
